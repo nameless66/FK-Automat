00247d588844d7186f97d44d91d7abb481a6ad9e
baumgaja <baumgaja@as1.win.fh-albsig.de>
2014-11-27 12:01:22 +0100

    NoSuchKunde und KundeManagerInterfaceerstellt

---
+++ FKAutomatBeans/ejbModule/session/KundeManagerInterface.java
@@ -0,0 +1,26 @@
+package session;
+
+import java.util.Collection;
+
+import javax.ejb.Remote;
+import javax.ejb.Remove;
+
+
+import model.Kunde;
+
+@Remote
+public interface KundeManagerInterface {
+
+   public Collection<Kunde> list();
+
+   public Collection<Kunde> findByKid(long id);
+
+   public void delete(long primaryKey) throws NoSuchKunde;
+
+   public void save(Kunde k);
+
+ 
+
+   @Remove
+   void checkout();
+}
---
+++ FKAutomatBeans/ejbModule/session/NoSuchKunde.java
@@ -0,0 +1,5 @@
+package session;
+
+public class NoSuchKunde extends Exception {
+	private static final long serialVersionUID = 1538599602930372313L;
+}

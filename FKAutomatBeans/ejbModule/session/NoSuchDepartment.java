e9349bb90a40c88820aef143ea24c9da76685314
baumgaja <baumgaja@as1.win.fh-albsig.de>
2014-12-04 11:27:32 +0100

    anpassungen an den restlichen Managerklasse entfernen des alten Beispiels department-employee

--- FKAutomatBeans/ejbModule/session/NoSuchDepartment.java
+++
@@ -1,6 +0,0 @@
-package session;
-
-
-public class NoSuchDepartment extends Exception {
-	private static final long serialVersionUID = 3730898290187685568L;
-}
--- FKAutomatBeans/ejbModule/session/NoSuchEmployee.java
+++
@@ -1,5 +0,0 @@
-package session;
-
-public class NoSuchEmployee extends Exception {
-	private static final long serialVersionUID = 1538599602930372313L;
-}
